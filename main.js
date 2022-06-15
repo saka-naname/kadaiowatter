f6526aad32eb82022f212bb92e4b4c9b3b140cfd
yudai1204 <59430508+yudai1204@users.noreply.github.com>
2022-06-16 02:59:29 +0900

    verybigOwatta

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
+const mode = "backgroundOwatta";
 const contentsComplete = document.querySelector(".contents-detail.contents-complete");
 if(contentsComplete){
-    const elementKadaiOwatta = document.createElement("img");
-    elementKadaiOwatta.src = `chrome-extension://${chrome.runtime.id}/img/kadaiowatta.png`;
-    elementKadaiOwatta.classList = "kadaiowatta";
-    const kadaiOwattaContainer = document.createElement("div");
-    kadaiOwattaContainer.classList = "kadaiowatta-container";
-    kadaiOwattaContainer.appendChild(elementKadaiOwatta);
-    document.querySelector(".contents-title").appendChild(kadaiOwattaContainer);
-    document.querySelector(".block").remove();
+    if(mode === "backgroundOwatta"){
+        document.head.insertAdjacentHTML("beforeEnd",`
+        <style>
+        #pageMain{
+            background-image: url(chrome-extension://${chrome.runtime.id}/img/kadaiowatta.png);
+            background-size: cover;
+        }
+        .course-header {
+            background-color: #FFF7;
+            box-shadow: none;
+        }
+        .login-view {
+            background: none;
+        }
+        .page-foot {
+            background-color: #ECEBEA44;
+        }
+        #page_head{
+            backdrop-filter: blur(10px);
+            background-image: linear-gradient(180deg, #e8ebf078, #ffffff94);
+            background-color: transparent;
+        }
+        </style>
+        `);
+    }else{
+        const elementKadaiOwatta = document.createElement("img");
+        elementKadaiOwatta.src = `chrome-extension://${chrome.runtime.id}/img/kadaiowatta.png`;
+        elementKadaiOwatta.classList = "kadaiowatta";
+        const kadaiOwattaContainer = document.createElement("div");
+        kadaiOwattaContainer.classList = "kadaiowatta-container";
+        kadaiOwattaContainer.appendChild(elementKadaiOwatta);
+        document.querySelector(".contents-title").appendChild(kadaiOwattaContainer);
+        document.querySelector(".block").remove();
+    }
 }
